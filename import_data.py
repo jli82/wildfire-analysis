ddd3bcc47664dc269caa21461ace6e4ef8999dc5
Jake Li <lijake2001@gmail.com>
2024-04-11 14:13:06 -0500

    move code

--- import_data.py
+++ import_data.py
@@ -1,9 +1,15 @@
+# import library
 import pandas as pd
 import sqlite3
 
+# get wildfire data into dataframe
 conn = sqlite3.connect('wildfire.sqlite')
 wildfire_df = pd.read_sql_query('SELECT * FROM Fires', conn)
-wildfire_df.to_csv('wildfire.csv', index=False)
-
+# get weather data into dataframe
 weather_df = pd.read_csv('US_wildfire_weather_data.cvs')
-weather_df.to_csv('weather.csv', index=False)
+
+# merge datasets
+merged_df = wildfire_df.merge(weather_df, on='OBJECTID')
+merged_df.columns = merged_df.columns.str.lower()
+
+merged_df.to_csv('merged_df.csv', index=False)
